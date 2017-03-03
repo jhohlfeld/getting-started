a07f039964e8bc67b8a632183b23deec6de6f899
Henning Peters <pete@dexterslab.de>
2017-03-03 15:09:01 +0100

    add demo

---
+++ demo.py
@@ -0,0 +1,14 @@
+import os
+import io
+
+import riseml.server
+from PIL import Image
+
+def transform(in_bytes):
+  in_img = Image.open(io.BytesIO(in_bytes))
+  out_img = in_img.rotate(180)
+  out_bytes = io.BytesIO()
+  out_img.save(out_bytes, 'JPEG')
+  return out_bytes.getvalue()
+
+riseml.server.serve(transform, port=os.environ.get('PORT'))
